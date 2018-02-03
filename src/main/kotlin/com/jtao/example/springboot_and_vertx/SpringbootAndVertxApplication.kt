ac0746fbc002be4ff5b30938b1bda88c6268038f
JTaoZh <954463838@qq.com>
2018-02-03 22:55:17 +0800

    hello world

---
+++ src/main/kotlin/com/jtao/example/springboot_and_vertx/SpringbootAndVertxApplication.kt
@@ -0,0 +1,29 @@
+package com.jtao.example.springboot_and_vertx
+
+import io.vertx.core.AbstractVerticle
+import io.vertx.core.Vertx
+import org.slf4j.LoggerFactory
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+
+@SpringBootApplication
+class SpringbootAndVertxApplication{
+    @Bean("vertx")
+    fun vertx() = Vertx.vertx()
+}
+
+class DemoVerticle: AbstractVerticle(){
+
+    private val log = LoggerFactory.getLogger(DemoVerticle::class.java)!!
+
+    override fun start(){
+        log.info("Hello world")
+    }
+}
+
+fun main(args: Array<String>) {
+    val ctx = SpringApplication.run(SpringbootAndVertxApplication::class.java, *args)
+    val vertx = ctx.getBean("vertx") as Vertx
+    vertx.deployVerticle(DemoVerticle())
+}
---
+++ src/test/kotlin/com/jtao/example/springboot_and_vertx/SpringbootAndVertxApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.jtao.example.springboot_and_vertx
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpringbootAndVertxApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
